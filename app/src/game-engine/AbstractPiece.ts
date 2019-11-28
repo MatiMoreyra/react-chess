c80c9c5e0e875110c971ff08e9920b0b6091f455
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-11-28 17:10:28 -0300

    Add AbstractPiece

---
+++ app/src/game-engine/AbstractPiece.ts
@@ -0,0 +1,20 @@
+import { ChessSquare } from "./ChessSquare";
+import { ChessBoard } from "./ChessBoard";
+
+export enum PieceColor {
+    Black, 
+    White,
+}
+
+export abstract class AbstractPiece {
+    private _color: PieceColor;
+    constructor(color: PieceColor) {
+        this._color = color;
+    }
+    
+    public get color() : PieceColor {
+        return this._color;
+    }
+    
+    public abstract canMove(source: ChessSquare, destination: ChessSquare, board: ChessBoard): boolean;
+}
