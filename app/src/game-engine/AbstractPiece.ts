f3bc5d3196aee8a0750b1a0f8dfce4563c01f9f5
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-12-02 00:28:03 -0300

    Cosmetic changes

--- app/src/game-engine/AbstractPiece.ts
+++ app/src/game-engine/AbstractPiece.ts
@@ -2,19 +2,25 @@ import { ChessSquare } from "./ChessSquare";
 import { ChessBoard } from "./ChessBoard";
 
 export enum PieceColor {
-    Black, 
-    White,
+  Black,
+  White
 }
 
 export abstract class AbstractPiece {
-    private _color: PieceColor;
-    constructor(color: PieceColor) {
-        this._color = color;
-    }
-    
-    public get color() : PieceColor {
-        return this._color;
-    }
-    
-    public abstract canMove(source: ChessSquare, destination: ChessSquare, board: ChessBoard): boolean;
+  private _color: PieceColor;
+  constructor(color: PieceColor) {
+    this._color = color;
+  }
+
+  public get color(): PieceColor {
+    return this._color;
+  }
+
+  public abstract canMove(
+    source: ChessSquare,
+    destination: ChessSquare,
+    board: ChessBoard
+  ): boolean;
+
+  public abstract name(): string;
 }
--- app/src/game-engine/ChessSquare.ts
+++ app/src/game-engine/ChessSquare.ts
@@ -1,34 +1,31 @@
-import assert from "assert";
-import { AbstractPiece } from "./AbstractPiece";
-
 // Represents a chess square, used as a container for a chess piece.
 export class ChessSquare {
-    private _row!: number;
-    private _column!: number;
+  private _row!: number;
+  private _column!: number;
 
-    constructor(row: number, column: number) {
-        // Validate parameters
-        if (column > 7 || column < 0) {
-            throw new Error('Invalid column number');
-        }
-        if (row > 7 || row < 0) {
-            throw new Error('Invalid row number');
-        }
-        this._row = row;
-        this._column = column;
+  constructor(row: number, column: number) {
+    // Validate parameters
+    if (column > 7 || column < 0) {
+      throw new Error("Invalid column number");
     }
+    if (row > 7 || row < 0) {
+      throw new Error("Invalid row number");
+    }
+    this._row = row;
+    this._column = column;
+  }
 
-    get row(): number {
-        return this._row;
-    }
+  get row(): number {
+    return this._row;
+  }
 
-    get column(): number {
-        return this._column;
-    }
+  get column(): number {
+    return this._column;
+  }
 
-    get label(): string {
-        let rowLabel = String.fromCharCode('a'.charCodeAt(0) + this.row);
-        let columnLabel = (this.column + 1).toString();
-        return rowLabel + columnLabel;
-    }
+  get label(): string {
+    let rowLabel = String.fromCharCode("a".charCodeAt(0) + this.row);
+    let columnLabel = (this.column + 1).toString();
+    return rowLabel + columnLabel;
+  }
 }
