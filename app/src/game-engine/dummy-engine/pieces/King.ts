44fd82da1461412841da10a91b83ca3c841f9079
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-12-03 02:35:35 -0300

    Fix King movement

--- app/src/game-engine/dummy-engine/pieces/King.ts
+++ app/src/game-engine/dummy-engine/pieces/King.ts
@@ -8,18 +8,17 @@ export class King extends AbstractPiece {
     destination: ChessSquare,
     board: ChessBoard
   ): boolean {
-    let pieceAtDestination = board.pieceAt(destination);
-
     // Cannot move if there is a piece of the same color at destination
+    let pieceAtDestination = board.pieceAt(destination);
     if (pieceAtDestination != null && pieceAtDestination.color === this.color) {
       return false;
     }
 
-    let dX = source.row - destination.row;
-    let dY = source.column - destination.column;
+    let dy = source.row - destination.row;
+    let dx = source.column - destination.column;
 
-    // Can move only 1 square away in any direction
-    if (Math.abs(dX) <= 1 && Math.abs(dY) <= 1) {
+    // Can move 1 square away in any direction
+    if (Math.abs(dx) <= 1 && Math.abs(dy) <= 1) {
       return true;
     }
     return false;
