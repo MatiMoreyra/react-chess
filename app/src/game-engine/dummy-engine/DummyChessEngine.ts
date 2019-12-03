71467f248f308fa15dff7099138fe1dbddb91518
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-12-03 02:35:09 -0300

    Implement DummyChessEngine.move

--- app/src/game-engine/dummy-engine/DummyChessEngine.ts
+++ app/src/game-engine/dummy-engine/DummyChessEngine.ts
@@ -2,6 +2,7 @@ import { AbstractChessEngine, Move } from "../AbstractChessEngine";
 import { AbstractPiece, PieceColor } from "../AbstractPiece";
 import { King } from "./pieces/King";
 import { ChessBoard } from "../ChessBoard";
+import { ChessSquare } from "../ChessSquare";
 
 export class DummyChessEngine extends AbstractChessEngine {
   private _pieces: Array<Array<AbstractPiece | null>>;
@@ -25,19 +26,40 @@ export class DummyChessEngine extends AbstractChessEngine {
   }
 
   public move(move: Move): boolean {
-    if (move.destination.equals(move.source)) {
-       return false;
+    if (this.isValidMove(move)) {
+      this.setPiece(move.destination, this.pieceAt(move.source));
+      this.setPiece(move.source, null);
+      return true;
     }
-    this._pieces[move.destination.row][move.destination.column] = this._pieces[
-      move.source.row
-    ][move.source.column];
-    this._pieces[move.source.row][move.source.column] = null;
-    return true;
+    return false;
   }
+
   public isValidMove(move: Move): boolean {
-    return true;
+    if (move.destination.equals(move.source)) {
+      return false;
+    }
+    let piece = this.pieceAt(move.source);
+    if (piece == null) {
+      return false;
+    }
+    if (piece.color !== this.whoPlays()) {
+      return false;
+    }
+    if (piece.canMove(move.source, move.destination, this.getChessBoard())) {
+      return true;
+    }
+    return false;
   }
+
   public whoPlays(): PieceColor {
     return PieceColor.White;
+  }
+
+  private pieceAt(square: ChessSquare): AbstractPiece | null {
+    return this._pieces[square.row][square.column];
+  }
+
+  private setPiece(square: ChessSquare, piece: AbstractPiece | null) {
+    this._pieces[square.row][square.column] = piece;
   }
 }
