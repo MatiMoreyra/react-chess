fcf29930c92da3a8e3e8fc49ef600e131e0aa4b7
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-12-04 10:21:42 -0300

    Add a FEN parser

--- app/src/game-engine/dummy-engine/DummyChessEngine.ts
+++ app/src/game-engine/dummy-engine/DummyChessEngine.ts
@@ -1,36 +1,20 @@
 import { AbstractChessEngine, Move } from "../AbstractChessEngine";
 import { AbstractPiece, PieceColor } from "../AbstractPiece";
-import { King } from "./pieces/King";
 import { ChessBoard } from "../ChessBoard";
 import { ChessSquare } from "../ChessSquare";
-import { Rook } from "./pieces/Rook";
-import { Pawn } from "./pieces/Pawn";
-import { Queen } from "./pieces/Queen";
-import { Bishop } from "./pieces/Bishop";
-import { Knight } from "./pieces/Knight";
+import { parseFen } from "./FenParser";
 
 export class DummyChessEngine extends AbstractChessEngine {
   private _pieces: Array<Array<AbstractPiece | null>>;
   private _history: Array<Move>;
   constructor() {
     super();
-    this._pieces = new Array<Array<AbstractPiece | null>>(8);
-    for (let row = 0; row < 8; row++) {
-      this._pieces[row] = new Array<AbstractPiece | null>(8).fill(null);
+    let pieces = parseFen("rnbqkbnr/pp1pp1pp/5B2/2p1P1p1/2P5/2N1N3/PP1P1PPP/R2QKB1R");
+    if (pieces != null) {
+      this._pieces = pieces;
+    } else {
+      throw new Error("Invalid fen");
     }
-    this._pieces[0][1] = new King(PieceColor.White);
-    this._pieces[0][2] = new Queen(PieceColor.White);
-    this._pieces[0][3] = new Rook(PieceColor.White);
-    this._pieces[0][4] = new Bishop(PieceColor.White);
-    this._pieces[0][5] = new Knight(PieceColor.White);
-    this._pieces[0][6] = new Pawn(PieceColor.White);
-
-    this._pieces[7][1] = new King(PieceColor.Black);
-    this._pieces[7][2] = new Queen(PieceColor.Black);
-    this._pieces[7][3] = new Rook(PieceColor.Black);
-    this._pieces[7][4] = new Bishop(PieceColor.Black);
-    this._pieces[7][5] = new Knight(PieceColor.Black);
-    this._pieces[7][6] = new Pawn(PieceColor.Black);
     this._history = new Array<Move>();
   }
 
