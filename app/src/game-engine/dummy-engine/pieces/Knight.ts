16e1c7081ab80d21ef2f9f16a2ef0a0379d11cf8
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-12-05 18:28:57 -0300

    Implement Knight movement

--- app/src/game-engine/dummy-engine/pieces/Knight.ts
+++ app/src/game-engine/dummy-engine/pieces/Knight.ts
@@ -8,7 +8,23 @@ export class Knight extends AbstractPiece {
     destination: ChessSquare,
     board: ChessBoard
   ): boolean {
-    return true;
+    // Cannot move if there is a piece of the same color at destination
+    let pieceAtDestination = board.pieceAt(destination);
+    if (pieceAtDestination != null && pieceAtDestination.color === this.color) {
+      return false;
+    }
+  
+    let dx = destination.column - source.column;
+    let dy = destination.row - source.row;
+  
+    if (Math.abs(dx) === 1 && Math.abs(dy) === 2) {
+      return true;
+    }
+    if (Math.abs(dx) === 2 && Math.abs(dy) === 1) {
+      return true;
+    }
+  
+    return false;
   }
 
   public name(): string {
