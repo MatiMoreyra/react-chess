091fa8c39b320fa13f1381d4d39f3c03521e8319
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-12-05 18:29:11 -0300

    Implement Queen movement

--- app/src/game-engine/dummy-engine/pieces/Queen.ts
+++ app/src/game-engine/dummy-engine/pieces/Queen.ts
@@ -1,14 +1,29 @@
-import { AbstractPiece } from "../../AbstractPiece";
+import { AbstractPiece, PieceColor } from "../../AbstractPiece";
 import { ChessSquare } from "../../ChessSquare";
 import { ChessBoard } from "../../ChessBoard";
+import { Bishop } from "./Bishop";
+import { Rook } from "./Rook";
 
 export class Queen extends AbstractPiece {
+  private _bishop: Bishop;
+  private _rook: Rook;
+
+  constructor(color: PieceColor) {
+    super(color);
+    this._bishop = new Bishop(color);
+    this._rook = new Rook(color);
+  }
+
   public canMove(
     source: ChessSquare,
     destination: ChessSquare,
     board: ChessBoard
   ): boolean {
-    return true;
+    // Combines bishop and rook movement.
+    return (
+      this._bishop.canMove(source, destination, board) ||
+      this._rook.canMove(source, destination, board)
+    );
   }
 
   public name(): string {
