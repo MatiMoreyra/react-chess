52f8cc6a3984b9c08a6f3ab7e6abfdd07df85a30
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-12-14 13:51:11 -0300

    Add Move class

---
+++ app/src/game-engine/local-engine/Move.ts
@@ -0,0 +1,32 @@
+import { Square } from "../ISquare";
+import { IMove } from "../IMove";
+
+// Class that provides extension methods to manipulate an IMove
+export class Move implements IMove {
+  source: Square;
+  destination: Square;
+  constructor(move: IMove) {
+    this.source = move.source;
+    this.destination = move.destination;
+  }
+
+  public get dx(): number {
+    return this.destination.column - this.source.row;
+  }
+
+  public get dy(): number {
+    return this.destination.row - this.source.row;
+  }
+
+  public isVertical(): boolean {
+    return this.dx === 0 && this.dy !== 0;
+  }
+
+  public isHorizontal(): boolean {
+    return this.dy === 0 && this.dx !== 0;
+  }
+
+  public isDiagonal(): boolean {
+    return Math.abs(this.dx) === Math.abs(this.dy) && this.dx !== 0;
+  }
+}
