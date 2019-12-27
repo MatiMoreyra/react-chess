4d6e89eac9a16faf132e2292f1a1d39f124db8b5
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-12-27 11:23:06 -0300

    Remove unneeded files

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { ChessGame } from './components/ChessGame';
 import { LocalEngine } from './game-engine/local-engine/LocalEngine';
