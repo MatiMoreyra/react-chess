d5e6394efebd7d11d0db07a02825625132ffea4c
Matias Moreyra <matias.a.moreyra@gmail.com>
2019-12-27 13:33:48 +0000

    Updates

---
+++ precache-manifest.82c0851ea83fd671bbafd945f961c0e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb00d27a63802e70f6dcc3a5857096f",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "046acf41207ddc19a873",
+    "url": "/react-chess/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "574e790679d1a042d683",
+    "url": "/react-chess/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "046acf41207ddc19a873",
+    "url": "/react-chess/static/js/2.0388b552.chunk.js"
+  },
+  {
+    "revision": "574e790679d1a042d683",
+    "url": "/react-chess/static/js/main.b69223dd.chunk.js"
+  },
+  {
+    "revision": "a1644039b973a5064926",
+    "url": "/react-chess/static/js/runtime-main.544795f9.js"
+  }
+]);
